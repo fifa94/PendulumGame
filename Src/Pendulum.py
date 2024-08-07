3fd94d7bb22d90b3a2a736cbb2f8aa425ed6c099
Filip Šrámek <sramek.filip@gmail.com>
2024-08-07 19:48:35 +0200

    feat[Pendulum] Added linear model

--- Src/Pendulum.py
+++ Src/Pendulum.py
@@ -28,7 +28,31 @@ class Pendulum_nonlinear_model:
 
         return theta, omega, t
 
+class Pendulum_linear_model:
 
+    def __init__(self, g, l, b):
+        self.g = g
+        self.l = l
+        self.b = b
+
+        # Definice systému diferenciálních rovnic
+    def equations_linear(self, y, t):
+        theta, omega = y
+        dtheta_dt = omega
+        domega_dt = - (self.g / self.l) * (theta) - self.b * omega
+        return [dtheta_dt, domega_dt]
+    
+    def run (self, phi0, omega0, t0, tf, dt):
+        t = np.arange(t0, tf, dt)
+        theta = np.zeros_like(t)
+        omega = np.zeros_like(t)
+        y0 = [phi0, omega0]
+        theta[0] = phi0
+        omega[0] = omega0
+
+        theta, omega = odeint(self.equations_linear, y0, t).T
+
+        return theta, omega, t
 
 if __name__ == "__main__":
 
@@ -47,6 +71,8 @@ if __name__ == "__main__":
     pendulum = Pendulum_nonlinear_model(g, l, b)
     angle, angular_vel, time = pendulum.run(phi0, omega0, t0, tf, dt)
 
+    pendulum_lin = Pendulum_linear_model(g, l, b)
+    angle_lin, angular_vel_lin, time_lin = pendulum_lin.run(phi0, omega0, t0, tf, dt)
     # Graf
     plt.figure(figsize=(10, 5))
     plt.plot(time, angle, label='Úhel (theta)')
@@ -57,3 +83,13 @@ if __name__ == "__main__":
     plt.title('Pohyb kyvadla (nelineární model)')
     plt.grid(True)
     plt.savefig('pendulum_simulation.png')  # Uložení grafu do souboru
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(time_lin, angle_lin, label='Úhel (theta)')
+    plt.plot(time_lin, angular_vel_lin, label='Úhlová rychlost (omega)')
+    plt.xlabel('Čas (s)')
+    plt.ylabel('Hodnota')
+    plt.legend()
+    plt.title('Pohyb kyvadla (lineární model)')
+    plt.grid(True)
+    plt.savefig('pendulum_simulation_lin.png')  # Uložení grafu do souboru
